d2b978f5be2a124a089651ce277704c3e3055429
letownia <borglq@gmail.com>
2018-05-04 11:56:03 +0200

    infoWindow autocloses on mobile

--- connect_to_proxy.sh
+++ connect_to_proxy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-./mysql -u root -p --host 127.0.0.1
+mysqlExe=/c/progs/mysql-8.0.11-winx64/bin/mysql.exe
+
+# Password is the same as in application.properties :
+# i.e. spring.datasource.password=epodroznik
+password=epodroznik
+
+$mysqlExe -u root -p$password --host 127.0.0.1
--- start_proxy.sh
+++ start_proxy.sh
@@ -4,4 +4,8 @@ new_db_name=mapdecorator:europe-west3:map-decorator-db
 old_db_name=mapdecorator:europe-west4:mapdecorator-db
 port_number=3306
 
+
+# cloud_proxy needs a service account key to connect to database.
+# Most recently it's found at C:\Users\letownia\mapDecorator-dfe83fa7f774.json
+# and set with environment variable
 $cloud_proxy_exe -instances=$new_db_name=tcp:$port_number
